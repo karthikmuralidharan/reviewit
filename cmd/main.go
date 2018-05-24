52dd1785f1785ec74501467a5e351a99677f078e
Karthik Muralidharan <mkarthik91@gmail.com>
2018-05-24 20:41:04 +0530

    use env variables for input

--- cmd/main.go
+++ cmd/main.go
@@ -5,30 +5,33 @@ import (
 	"log"
 	"os"
 
+	"github.com/spf13/viper"
+
 	"github.com/etherlabsio/reviewit"
 	"github.com/etherlabsio/reviewit/provider/gitlab"
 	"github.com/etherlabsio/reviewit/transport/slack"
-	"github.com/spf13/pflag"
 )
 
 func main() {
-	var (
-		providerToken = pflag.String("gitlab.token", "", "Version control provider access token")
-		group         = pflag.String("gitlab.group", "", "Name of the group to search projects under gitlab, github etc.")
-		slackWebhook  = pflag.String("slack.webhook", "", "Webhook URL for slack's bot")
-		slackChannel  = pflag.String("slack.channel", "", "Name of the slack channel to publish")
-	)
-	pflag.Parse()
+	viper.AutomaticEnv()
 
-	pflag.VisitAll(func(f *pflag.Flag) {
-		if f.DefValue == "" {
-			fmt.Println(f.Name + " is not set. \n")
+	var (
+		providerToken = viper.GetString("GITLAB_TOKEN")
+		group         = viper.GetString("GITLAB_GROUP")
+		slackWebhook  = viper.GetString("SLACK_WEBHOOK_URL")
+		slackChannel  = viper.GetString("SLACK_CHANNEL")
+	)
+
+	requiredInput := []string{"GITLAB_TOKEN", "GITLAB_GROUP", "SLACK_WEBHOOK_URL", "SLACK_CHANNEL"}
+	for _, k := range requiredInput {
+		if !viper.IsSet(k) {
+			fmt.Println(k + " is not set")
 			os.Exit(1)
 		}
-	})
+	}
 
-	provider := gitlab.NewGroupFilter(*group, *providerToken)
-	transport, err := slack.New(*slackChannel, *slackWebhook)
+	provider := gitlab.NewGroupFilter(group, providerToken)
+	transport, err := slack.New(slackChannel, slackWebhook)
 	if err != nil {
 		log.Fatal(err)
 	}
